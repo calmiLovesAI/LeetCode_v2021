6f0a4b7d5a111e286cf099066ff36218abf368e8
calmisential <xinyu_std@163.com>
2021-09-26 12:06:19 +0800

    有障碍物的路径题

---
+++ 0063_不同路径II.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[1 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                # 判断是否为障碍物
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                    continue
+                if i == 0 and j == 0:
+                    dp[i][j] = 1
+                elif i == 0:
+                    dp[i][j] = dp[i][j - 1]
+                elif j == 0:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[m - 1][n - 1]
