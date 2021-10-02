9719d40077753599b06fa00a00ec0466fd62c5a5
calmisential <xinyu_std@163.com>
2021-10-02 18:18:30 +0800

    Add

---
+++ 1436_旅行终点站.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        citiesA = {path[0] for path in paths}
+        return next(path[1] for path in paths if path[1] not in citiesA)
+
