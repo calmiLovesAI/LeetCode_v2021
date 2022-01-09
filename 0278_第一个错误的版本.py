17280b459d8c4597f8dc22a9e528a86d1fa12949
calmisential <xinyu_std@163.com>
2022-01-09 19:48:00 +0800

    二分查找

---
+++ 0278_第一个错误的版本.py
@@ -0,0 +1,25 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r = 1, n
+        while l <= r:
+            mid = (l + r) // 2
+            if isBadVersion(mid):
+                r = mid - 1
+                if 1 < mid <= n and not isBadVersion(mid-1):
+                    return mid
+            else:
+                l = mid + 1
+
+        if mid == 1 and isBadVersion(mid):
+            return 1
+
+            
