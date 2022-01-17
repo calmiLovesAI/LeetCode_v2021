dbc228687563be3445b4d8fc8e3182b4e129f465
calmisential <xinyu_std@163.com>
2022-01-17 21:28:16 +0800

    Update

---
+++ 矩阵/0542_01矩阵.py
@@ -0,0 +1,27 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        dist = [[0] * n for _ in range(m)]
+        zeroes_pos = [(i, j) for i in range(m) for j in range(n) if mat[i][j] == 0]
+        # 将所有的 0 添加进初始队列中
+        q = collections.deque(zeroes_pos)
+        seen = set(zeroes_pos)
+
+        # 广度优先搜索
+        while q:
+            i, j = q.popleft()
+            for ni, nj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                if 0 <= ni < m and 0 <= nj < n and (ni, nj) not in seen:
+                    dist[ni][nj] = dist[i][j] + 1
+                    q.append((ni, nj))
+                    seen.add((ni, nj))
+
+        return dist
+
+
+if __name__ == '__main__':
+    print(Solution().updateMatrix([[0, 0, 0], [0, 1, 0], [1, 1, 1]]))
