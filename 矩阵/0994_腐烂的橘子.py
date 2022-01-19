24269cc1b3c6f597eea191a5a5879120be7daa97
calmisential <xinyu_std@163.com>
2022-01-19 13:20:36 +0800

    广度优先搜索

---
+++ 矩阵/0994_腐烂的橘子.py
@@ -0,0 +1,42 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        # BFS
+        # 找出所有的"2"，然后放入队列
+        m = len(grid)
+        n = len(grid[0])
+        q = deque()
+        t = -1
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    q.append([i, j])
+
+        while q:
+            size = len(q)
+            for k in range(size):
+                i, j = q.popleft()
+                directions = ([i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1])
+                for d in directions:
+                    if 0 <= d[0] < m and 0 <= d[1] < n and grid[d[0]][d[1]] == 1:
+                        grid[d[0]][d[1]] = 2
+                        q.append(d)
+            t += 1
+
+        # 判断是否还剩余1
+        flag = True
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    flag = False
+
+        if not flag:
+            return -1
+        return t
+
+
+if __name__ == '__main__':
+    print(Solution().orangesRotting(grid=[[0, 2]]))
